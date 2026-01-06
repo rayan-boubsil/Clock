01b4e950c033e308f6222600a03c4d14245bba2c
logann-grange <logann.grange@laplateforme.io>
2026-01-06 10:21:15 +0100

    modif du réglage d'heure avec keyboard

--- Clock.py
+++ Clock.py
@@ -1,6 +1,9 @@
 import time
 from datetime import datetime, timedelta
+import keyboard
 
+hour = None
+able_print = True
 
 ##========= Afficher heure =========##
 def afficher_heure(hour=None) :
@@ -11,34 +14,40 @@ def afficher_heure(hour=None) :
         str_time = hour[0] + ':' + hour[1] + ':' + hour[2]
         date_time = datetime.strptime(str_time, "%H:%M:%S")
         time = str(date_time + timedelta(seconds=1)).split()[1]
-    print(time)
+    if able_print :
+        print(time)
     tuple_time = (time.split(':')[0], time.split(':')[1], time.split(':')[2])
     return tuple_time
 
+##======== Réglage de l'heure ========##
+def change_time(_event=None) :
+    global able_print
+    global hour
+    if _event is not None :
+        able_print = False
+        print("\n========== REGLAGE DE L'HEURE ===========")
+        new_h = input("Heure : ")
+        new_m = input("Minute : ")
+        new_s = input("Seconde : ")
+        hour = (new_h, new_m, new_s)
+        able_print = True
+        return True
 
-hour = None
-print("1 - Afficher l'heure\n2 - Régler l'heure\n3 - Ajouter une alarme")
-mode = input("Saisire le num de l'action : ")
+
+keyboard.on_release_key('c', change_time)
+running = True
 
 ##======== Mode 3 : Alarme ===========##
-if mode == '3' :
-    print('\n=============== ALARME =================')
-    h_alarm = input("Heure : ")
-    m_alarm = input("Minute : ")
-    s_alarm = input("Seconde : ")
-    alarm_time = (h_alarm, m_alarm, s_alarm)
+##if mode == '3' :
+##    print('\n=============== ALARME =================')
+##    h_alarm = input("Heure : ")
+##    m_alarm = input("Minute : ")
+##    s_alarm = input("Seconde : ")
+##    alarm_time = (h_alarm, m_alarm, s_alarm)
 
-##======== Mode 2 : Réglage de l'heure ========##
-if mode == '2' :
-    print("\n========== REGLAGE DE L'HEURE ===========")
-    h = input("Heure : ")
-    m = input("Minute : ")
-    s = input("Seconde : ")
-    hour = (h, m, s)
-    mode = '1'
-
-##======== Mode 1 : Affichage de l'heure actuel ==========##
-while mode == '1' :
+##======== Mode 1 : Affichage de l'heure ==========##
+while running == '1' :
     hour = afficher_heure(hour)
+    change_time()
     ##alarm(alarm_time)
     time.sleep(1)
