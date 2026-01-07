d55a015a920437661bd6e1d4a2fd90e055354e12
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-01-07 09:47:20 +0100

    rajout de reglage heure et rajout de securité de reglage alarme

--- horloge.py
+++ horloge.py
@@ -9,33 +9,43 @@ arlmeH=None
 time_zone=None
 paused=False
 modifier_alarme=False
+modif_heure=False
 
+# Afficher l'heure actuelle
 def afficher_heure(time=None,time_zone=None):
-    # Affiche l'heure actuelle au format  24h
-    if  time_zone == 24:  
-        if time is not None:
-            actual_time = time
-        else:
-            # Obtenir l'heure et la date actuelle
-            t = datetime.datetime.now() 
-            # Extraire heures, minutes, secondes
-            actual_time = (t.hour, t.minute, t.second)
+    if time is None:
+        # Obtenir l'heure et la date actuelle
+        t = datetime.datetime.now() 
+        actual_time = (t.hour, t.minute, t.second)
+    else:
+        # Incrémenter l'heure fournie d'une seconde
+        h, m, s = time
+        s += 1
+        if s >= 60:
+            s = 0
+            m += 1
+            if m >= 60:
+                m = 0
+                h += 1
+                if h >= 24:
+                    h = 0
+        actual_time = (h, m, s)
+    
+    # Afficher selon le format
+    if time_zone == 24:
         print(f'{actual_time[0]}:{actual_time[1]}:{actual_time[2]}')
-        return actual_time  
-    # Affiche l'heure actuelle au format 12h
     elif time_zone == 12:
-        if time is not None:
-            actual_time = time
-        else:
-            t = datetime.datetime.now() 
-            hour = t.hour
-            if hour > 12:
-                hour -= 12
-            actual_time = (hour, t.minute, t.second)
-        print(f'{actual_time[0]}:{actual_time[1]}:{actual_time[2]} {t.strftime("%p")}')
-        return actual_time
+        hour = actual_time[0]
+        ampm = "AM" if hour < 12 else "PM"
+        if hour == 0:
+            hour = 12
+        elif hour > 12:
+            hour -= 12
+        print(f'{hour}:{actual_time[1]}:{actual_time[2]} {ampm}')
     
+    return actual_time
     
+# Mettre en pause ou reprendre l'horloge
 def pause(_event=None):
     global paused
     if _event is not None:
@@ -43,16 +53,15 @@ def pause(_event=None):
         return 
     while paused:
         time.sleep(0.1)
-  
-  
+   
+# Gérer l'alarme
 def alarme(time=None, _event=None):
     global arlmeH, modifier_alarme, runnig
     
     # Si appelé par hotkey 'r', time sera un KeyboardEvent
     if time is not None and hasattr(time, 'event_type'):
         modifier_alarme = True
-        return
-    
+        return   
     # Si time est None, c'est pour régler l'alarme
     if time is None:
         arlmeH=None
@@ -64,6 +73,10 @@ def alarme(time=None, _event=None):
                     arlmeH=None
                     continue
                 arlmeh = (int(arlmeh[0:2]), int(arlmeh[3:5]), int(arlmeh[6:8]))
+                if arlmeh[0]>23 or arlmeh[1]>59 or arlmeh[2]>59:
+                    print("Heure invalide pour le format 24h. Veuillez réessayer.")
+                    arlmeH=None
+                    continue
                 arlmeH = arlmeh
             elif arlmeH is None and time_zone==12:
                 arlmeh=input("Entrez l'heure de l'alarme (HH:MM:SS AM/PM) : ")
@@ -72,33 +85,68 @@ def alarme(time=None, _event=None):
                     arlmeH=None
                     continue
                 hour=int(arlmeh[0:2])
+                if hour>12 or int(arlmeh[3:5])>59 or int(arlmeh[6:8])>59 or (arlmeh[9:]!="AM" and arlmeh[9:]!="PM"):
+                    print("Heure invalide pour le format 12h. Veuillez réessayer.")
+                    arlmeH=None
+                    continue
                 if arlmeh[9:]=="PM" and hour!=12:
                     hour+=12
                 elif arlmeh[9:]=="AM" and hour==12:
                     hour=0
                 arlmeh = (hour, int(arlmeh[3:5]), int(arlmeh[6:8]))
                 arlmeH = arlmeh
-        return
-    
+        return   
     # Sinon, vérifier si l'alarme sonne
-    if arlmeH is None:
+    if arlmeH and time_zone is not None:  
+        if time[0]==arlmeH[0] and time[1]==arlmeH[1] and time[2]==arlmeH[2]:
+            print("Il est l'heure !")
+            runnig=False
+            
+def regler_heur(_event=None,time=None,time_zone=None):
+    if _event is not None:
+        global modif_heure
+        modif_heure=True
         return
-    if time_zone==24:  
-        if time[0]==arlmeH[0] and time[1]==arlmeH[1] and time[2]==arlmeH[2]:
-            print("Il est l'heure !")
-            runnig=False
-    elif time_zone==12:                 
-        if time[0]==arlmeH[0] and time[1]==arlmeH[1] and time[2]==arlmeH[2]:
-            print("Il est l'heure !")
-            runnig=False
-
+    Ntime=None    
+    while Ntime is None:
+            if time_zone==24:
+                Ntime=input("Entrez la nouvelle heure (HH:MM:SS) : ")
+                if len(Ntime)!=8 or Ntime[2]!=":" or Ntime[5]!=":":
+                    print("Format incorrect. Veuillez réessayer.")
+                    continue
+                Ntime = (int(Ntime[0:2]), int(Ntime[3:5]), int(Ntime[6:8]))
+                if Ntime[0]>23 or Ntime[1]>59 or Ntime[2]>59:
+                    print("Heure invalide pour le format 24h. Veuillez réessayer.")
+                    Ntime=None
+                    continue
+            elif time_zone==12:
+                Ntime=input("Entrez la nouvelle heure (HH:MM:SS AM/PM) : ")
+                if len(Ntime)!=11 or Ntime[2]!=":" or Ntime[5]!=":" or Ntime[8]!=" ":
+                    print("Format incorrect. Veuillez réessayer.")
+                    Ntime=None
+                    continue
+                hour=int(Ntime[0:2])
+                if hour>12 or int(Ntime[3:5])>59 or int(Ntime[6:8])>59 or (Ntime[9:]!="AM" and Ntime[9:]!="PM"):
+                    print("Heure invalide pour le format 12h. Veuillez réessayer.")
+                    Ntime=None
+                    continue
+                if Ntime[9:]=="PM" and hour!=12:
+                    hour+=12
+                elif Ntime[9:]=="AM" and hour==12:
+                    hour=0
+                Ntime = (hour, int(Ntime[3:5]), int(Ntime[6:8]))
+    return Ntime
 
 def main():
     global arlmeH
     global time_zone
     global modifier_alarme
+    global modif_heure
+    actual_time=None
+    new_time=None
     keyboard.on_release_key('p', pause)
     keyboard.on_release_key('r', alarme)
+    keyboard.on_release_key('e', regler_heur)
     while runnig:
         if time_zone is None:
             tz=input("Choisissez le format d'heure (12/24) : ")
@@ -106,11 +154,20 @@ def main():
                 print("Format incorrect. Veuillez réessayer.")
                 continue
             time_zone=int(tz)
-        if arlmeH is None or modifier_alarme:
+        if modif_heure:
+            modif_heure=False
+            new_time=regler_heur(time_zone=time_zone)
+            actual_time=afficher_heure(time=new_time,time_zone=time_zone)    
+        if  modifier_alarme:
             modifier_alarme = False
             alarme()
-        actual_time = afficher_heure(time_zone=time_zone)
-        alarme(actual_time)
+        if actual_time is None:
+            actual_time = afficher_heure(time_zone=time_zone)
+        else:
+            actual_time = afficher_heure(time=actual_time,time_zone=time_zone)
+        
+        if arlmeH is not None: 
+            alarme(actual_time)
         time.sleep(1)
         pause()  
 if __name__ == "__main__":
