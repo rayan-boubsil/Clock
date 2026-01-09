a63f8ec625d0630ac7e8b418c0a75eadf9ef13d7
logann-grange <logann.grange@laplateforme.io>
2026-01-09 12:14:50 +0100

    lisibilité cu code

--- commun.py
+++ commun.py
@@ -3,13 +3,14 @@ from datetime import datetime, timedelta
 import keyboard
 
 ##====== Initialisation des variables globales ======##
-heure=None
-pause=False
-pouvoir_afficher=True
-heure_alarme=None
-heure_format=24
-en_saisie=False
+heure = None
+pause = False
+pouvoir_afficher = True
+heure_alarme = None
+heure_format = 24
+en_saisie = False
 hotkeys_actifs = []  # Liste pour stocker les hotkeys actifs
+
 
 ##======= rafraichissement de l'heure ========#
 def rafraichir_heure(heure=None) : 
@@ -25,26 +26,25 @@ def rafraichir_heure(heure=None) :
 
     return heure
 
+
 ##==== afficher heure =====##
 def afficher_heure(heure=None):
     heure_aff = heure
     # Affichage selon le format choisi ##
-    if heure_format==24:
-        am_pm=""
-    elif heure_format==12 and heure[0]>=12:
-        am_pm=" PM"
-        if heure[0]>12:
+    if heure_format == 24:
+        am_pm = ""
+    elif heure_format == 12 and heure[0] >= 12:
+        am_pm = "PM"
+        if heure[0] > 12:
             heure_aff = (heure[0]-12, heure[1], heure[2])
-    elif heure_format==12 and heure[0]<12:
-        am_pm=" AM"
-        if heure[0]==0:
+    elif heure_format == 12 and heure[0] < 12:
+        am_pm = "AM"
+        if heure[0] == 0:
             heure_aff = (12, heure[1], heure[2])
     ## Afficher l'heure ##
     if pouvoir_afficher:
         ligne_heure = f'|              \033[91m{heure_aff[0]:02d} : {heure_aff[1]:02d} : {heure_aff[2]:02d} {am_pm}\033[0m' + ' '*(15 - len(am_pm)) +'|'
         print(ligne_heure)
-
-    return 0
 
 
 #====== Réglage de l'heure ======##
@@ -78,12 +78,12 @@ def regler_heure():
             reactiver_hotkeys()
         
 
-##====Réglage de l'alarme=====##
+##==== Réglage de l'alarme =====##
 def regler_alarme():
     global heure_alarme, pouvoir_afficher,heure_format, en_saisie
     if not en_saisie:
-        reglage_en_cours=True
-        pouvoir_afficher=False
+        reglage_en_cours = True
+        pouvoir_afficher = False
         desactiver_hotkeys()
         en_saisie = True    
         print("\n===== Réglage de l'alarme ====")
@@ -97,7 +97,7 @@ def regler_alarme():
                     alarme_h = alarme_h + 12
                 elif am_pm == "AM" and alarme_h == 12:
                     alarme_h = "0"
-                 # Vérifier que si heure > 12 et AM, c'est invalide
+                # Vérifier que si heure > 12 et AM, c'est invalide
                 if int(alarme_h) > 12 and am_pm == "AM":
                     print("\n====Entrée invalide. Veuiller réessayer .=======")
                     continue    
@@ -106,7 +106,7 @@ def regler_alarme():
                 str(alarme_s).isdigit() and 0 <= alarme_s < 60):
                 heure_alarme = (alarme_h, alarme_m, alarme_s)
                 reglage_en_cours = False
-                pouvoir_afficher=True
+                pouvoir_afficher = True
             else:
                 print("\n====Entrée invalide. Veuillez réessayer.=======")
         en_saisie = False
@@ -116,7 +116,7 @@ def verifier_alarme(heure=None):
     global heure_alarme, pouvoir_afficher
     if heure_alarme is not None and heure is not None:
         if heure == heure_alarme and pouvoir_afficher:
-            pouvoir_afficher=True
+            pouvoir_afficher = True
             print("|  \33[92m===== ALARME ! ALARME ! ALARME ! =====\33[0m  |")
             heure_alarme = None  # Réinitialiser l'alarme après sonnerie
             return 0
@@ -124,8 +124,7 @@ def verifier_alarme(heure=None):
         print(f"|__________________________________________|")
 
 
-
-##=====changer format heure====##
+##===== changer format heure ====##
 def changer_format():
     global heure_format
     if not en_saisie:
@@ -134,49 +133,48 @@ def changer_format():
         else:
             heure_format = 24
 
-##====pause====##
+
+##======= pause =======##
 def pause_heure():
     global pause, pouvoir_afficher, en_saisie   
     if not en_saisie:
-        en_saisie=True
+        en_saisie = True
         desactiver_hotkeys()
         pause = not pause
         pouvoir_afficher = not pouvoir_afficher
-        en_saisie=False
-    reactiver_hotkeys()    
-        
+        en_saisie = False
+    reactiver_hotkeys()
+
+#===== desactiver les raccourcies clavier =====#
 def desactiver_hotkeys():
-    """Désactive tous les hotkeys"""
     global hotkeys_actifs
     for hk in hotkeys_actifs:
         keyboard.remove_hotkey(hk)
     hotkeys_actifs = []
 
 
+#===== activer les raccourcies clavier =====#
 def reactiver_hotkeys():
-    """Réactive tous les hotkeys"""
     global hotkeys_actifs
-    # D'abord s'assurer qu'ils sont bien désactivés
-    desactiver_hotkeys()
-    #time.sleep(0.2)
-    # Puis les réactiver
+    desactiver_hotkeys() # s'assurer qu'ils sont bien désactivés
+    # les réactiver
     hotkeys_actifs.append(keyboard.add_hotkey('f', changer_format))
     hotkeys_actifs.append(keyboard.add_hotkey('a', regler_alarme ))
     hotkeys_actifs.append(keyboard.add_hotkey('p', pause_heure))
     hotkeys_actifs.append(keyboard.add_hotkey('c', regler_heure))
         
-        
-###===== Boucle principale ======##
-
+       
+##===== Boucle principale ======##
 reactiver_hotkeys()
 marche=True
+
 while marche:
     if pouvoir_afficher :
         print('\n\n#==========================================#')
         print('|                                          |')
     heure=rafraichir_heure(heure)
     afficher_heure(heure)
-    verifier_alarme(heure=heure)
+    verifier_alarme(heure)
     if pouvoir_afficher :
         print('|\033[93mF\033[0m:format | \033[93mA\033[0m:alarme | \033[93mP\033[0m:pause | \033[93mC\033[0m:réglages|')
         print('#==========================================#')
